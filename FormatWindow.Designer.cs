d236a1bd4f20898d7884a273712c948aeee673b1
Kase, Misao <29609700+pic-man749@users.noreply.github.com>
2024-05-25 22:12:37 +0900

    [update]format description

--- FormatWindow.Designer.cs
+++ FormatWindow.Designer.cs
@@ -35,7 +35,7 @@
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.MinimumSize = new System.Drawing.Size(400, 80);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(525, 156);
+            this.label1.Size = new System.Drawing.Size(581, 351);
             this.label1.TabIndex = 1;
             this.label1.Text = resources.GetString("label1.Text");
             // 
@@ -43,9 +43,9 @@
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(12, 176);
+            this.button1.Location = new System.Drawing.Point(12, 376);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(560, 23);
+            this.button1.Size = new System.Drawing.Size(610, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "close";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,7 +55,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 211);
+            this.ClientSize = new System.Drawing.Size(634, 411);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "FormatWindow";
